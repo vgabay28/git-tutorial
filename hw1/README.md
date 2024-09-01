85e25c3fcce42df3fc932f3e7ca705e18dea3f75
Anne Rogers <amr@cs.uchicago.edu>
2024-09-01 14:09:39 -0500

    Add distribution for Homework 1

---
+++ hw1/README.md
@@ -0,0 +1,7 @@
+CMSC 14100
+Autumn 2024
+Homework #1
+
+hw1.py: You will do your work in this Python file.
+
+README.md: the file you are reading.
---
+++ hw1/hw1.py
@@ -0,0 +1,23 @@
+"""
+CMSC 14100
+Autumn 2024
+Homework #1
+
+YOUR NAME GOES HERE
+
+People Consulted:
+   For future assignments, you will be expected to list anyone (other than the
+   course staff that you consulted about this assignment).
+
+Online resources consulted:
+   For future assignments, you will be expected to list the URL of
+   any online resources that you use to complete this assignment.
+"""
+
+def add2():
+    ### Replace None with 2 + 2 in the next line.
+    return None
+
+if __name__ == "__main__":
+    print("2 + 2 is", add2())
+    
